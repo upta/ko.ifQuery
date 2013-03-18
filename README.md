86b18a0cc3107d134453792ce1fa37941d897aa0
Brian Uptagrafft <upta@outlook.com>
2013-03-18 13:14:45 -0700

    First upload of all the files
    Including first draft of readme.md

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-ko.ifQuery
-==========
+#ko.ifQuery
 
-A simple knockout binding that enables you to conditionally display elements using Media Queries
+A simple [knockout](https://www.knockoutjs.com) binding that enables you to conditionally display elements using Media Queries
+
+## What it does
+ko.ifQuery extends the idea of knockout's [if](http://knockoutjs.com/documentation/if-binding.html) binding to use Media Queries.  When used in conjunction with knockout templates, ko.ifQuery can also be used to selectively load different images based on screen size.
+
+You can check out the selective image loading in the [demo](http://ifQuery.upta.me/demo.html)
+
+## Simple sample
+```
+<!-- ko ifQuery: 'only screen and (max-width: 768px)' -->
+<div>I'm only in the dom if the window is 768px or less</div>
+<!-- /ko -->
+```
+
+## Default queries
+ko.ifQuery includes a set of default query values you can use (based off the excellent Zurb Foundation [visibility styles](http://foundation.zurb.com/docs/components/visibility.html))
+
+```
+<!-- ko ifQuery: 'small' -->
+<div>I'm only in the dom if 'small'</div>
+<!-- /ko -->
+```
+
+ * small
+ * medium-down
+ * medium
+ * medium-up
+ * large-down
+ * large-up
+ * large-down
+ * xlarge
+ * portrait
+ * landscape
+
+## Multiple rules
+You can also combine multiple rules together by comma-delimiting them.
+```
+<!-- ko ifQuery: 'small,landscape' -->
+<div>I'm only in the dom if 'small' and in landscape</div>
+<!-- /ko -->
+```
+
+## Caveats
+ko.ifQuery uses the [matchMedia(http://caniuse.com/#feat=matchmedia) function, which not all browsers support.  If you want to support these browsers, you'll need to use a polyfill such as [Modernizr](http://modernizr.com/). (note: I haven't tested this myself, your mileage may vary :)
---
+++ ko.ifQuery-1.0.0.js
@@ -0,0 +1,87 @@
+﻿( function ()
+{
+    var
+        bindingName = 'ifQuery',
+        cloneNodes = function ( nodesArray, shouldCleanNodes )  /* copied directly from ko.utils, since it doesn't expose it publicly */
+        {
+            for ( var i = 0, j = nodesArray.length, newNodesArray = []; i < j; i++ )
+            {
+                var clonedNode = nodesArray[i].cloneNode( true );
+                newNodesArray.push( shouldCleanNodes ? ko.cleanNode( clonedNode ) : clonedNode );
+            }
+
+            return newNodesArray;
+        },
+        domDataKey = '__ko_ifQueryBindingData',
+        queries =
+        {
+            "small": "(max-width: 767px)",
+            "medium-down": "(max-width: 1279px)",
+            "medium": "(min-width: 768px) and (max-width: 1279px)",
+            "medium-up": "(min-width: 768px)",
+            "large-down": "(max-width: 1439px)",
+            "large": "(min-width: 1280px) and (max-width: 1439px)",
+            "large-up": "(min-width: 1280px)",
+            "xlarge": "(min-width: 1440px)",
+            "landscape": "(orientation: landscape)",
+            "portrait": "(orientation: portrait)",
+        }; /* Size definitions based on http://foundation.zurb.com/docs/components/visibility.html */
+
+    ko.bindingHandlers[bindingName] =
+    {
+        'init': function ( element, valueAccessor, allBindingsAccessor, viewModel, bindingContext )
+        {
+            ko.utils.domData.set( element, domDataKey, {} );
+
+            return { 'controlsDescendantBindings': true };
+        },
+        'update': function ( element, valueAccessor, allBindingsAccessor, viewModel, bindingContext )
+        {
+            var ifQueryData = ko.utils.domData.get( element, domDataKey ),
+                dataValue = ko.utils.unwrapObservable( valueAccessor() ),
+                isFirstRender = !ifQueryData.savedNodes,
+                split = dataValue.split( "," ),
+                finalQuery = "";
+
+            for ( var i = 0; i < split.length; i++ )
+            {
+                var query = queries[split[i]] || split[i];
+
+                finalQuery += ( finalQuery ? " and " + query : query );
+            }
+
+            finalQuery = finalQuery.replace( "only screen and", "" );
+
+            var
+                shouldDisplay = matchMedia( finalQuery ).matches,
+                needsRefresh = isFirstRender || ( shouldDisplay !== ifQueryData.didDisplayOnLastUpdate );
+
+            if ( needsRefresh )
+            {
+                if ( isFirstRender )
+                {
+                    ifQueryData.savedNodes = cloneNodes( ko.virtualElements.childNodes( element ), true /* shouldCleanNodes */ );
+                }
+
+                if ( shouldDisplay )
+                {
+                    if ( !isFirstRender )
+                    {
+                        ko.virtualElements.setDomNodeChildren( element, cloneNodes( ifQueryData.savedNodes ) );
+                    }
+
+                    ko.applyBindingsToDescendants( bindingContext, element );
+                }
+                else
+                {
+                    ko.virtualElements.emptyNode( element );
+                }
+
+                ifQueryData.didDisplayOnLastUpdate = shouldDisplay;
+            }
+        }
+    };
+
+    ko.expressionRewriting.bindingRewriteValidators[bindingName] = false; // Can't rewrite control flow bindings
+    ko.virtualElements.allowedBindings[bindingName] = true;
+} )();
